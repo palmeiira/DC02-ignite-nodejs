395e18203d22d0a20bbf4755a6007dbea906a9dd
Thomaz <palmeiirat@gmail.com>
2021-05-17 23:55:35 -0400

    FIN: all tasks done

--- src/index.js
+++ src/index.js
@@ -23,20 +23,52 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
+
+// app.put("/repositories/:id", (request, response) => {
+//   const { id } = request.params;
+//   const { title, url, techs } = request.body;
+
+//   const changedRepository = repositories.find(repository => repository.id === id);
+
+//   if (!changedRepository) {
+//     return response.status(404).json({ error: "Repository not found" });
+//   }
+
+//   if (title) {
+//     changedRepository.title = title;
+//   }
+
+//   if (url) {
+//     changedRepository.url = url;
+//   }
+
+//   if (techs) {
+//     changedRepository.techs = techs;
+//   }
+
+//   return response.json(repository);
+// });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { url, title, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const repository = {
+    ...repositories[repositoryIndex],
+    url,
+    title,
+    techs
+  };
 
   repositories[repositoryIndex] = repository;
 
@@ -48,11 +80,11 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  repositories.splice(repositoryIndex, 1);
+  deletedObj = repositories.splice(repositoryIndex, 1)[0];
 
   return response.status(204).send();
 });
@@ -68,7 +100,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
